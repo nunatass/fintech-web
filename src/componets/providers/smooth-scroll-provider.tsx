3b55fe988abae349e5188a9f071808a10b77cfa2
Nuno Andrade <nunatass@gmail.com>
2025-12-16 07:34:48 +0000

    fix: remove invalid smoothTouch option from Lenis config

--- src/componets/providers/smooth-scroll-provider.tsx
+++ src/componets/providers/smooth-scroll-provider.tsx
@@ -17,7 +17,6 @@ export function SmoothScrollProvider({
       gestureOrientation: "vertical",
       smoothWheel: true,
       wheelMultiplier: 1,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
